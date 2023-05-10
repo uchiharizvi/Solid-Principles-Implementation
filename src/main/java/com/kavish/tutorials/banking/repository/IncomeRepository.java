890231c87ed9c38487e7b66e86400f1cabd9156d
Kavish <rizvikavish205@gmail.com>
2023-05-10 21:47:11 +0530

    Added Code

--- src/main/java/com/kavish/tutorials/banking/repository/IncomeRepository.java
+++ src/main/java/com/kavish/tutorials/banking/repository/IncomeRepository.java
@@ -1,4 +1,12 @@
 package com.kavish.tutorials.banking.repository;
 
+import com.kavish.tutorials.banking.model.Expense;
+import com.kavish.tutorials.banking.model.Income;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface IncomeRepository {
+    List<Income> getIncomes();
 }
--- src/main/java/com/kavish/tutorials/banking/service/CashFlowServiceImpl.java
+++ src/main/java/com/kavish/tutorials/banking/service/CashFlowServiceImpl.java
@@ -2,17 +2,24 @@ package com.kavish.tutorials.banking.service;
 
 import com.kavish.tutorials.banking.model.Expense;
 import com.kavish.tutorials.banking.model.Income;
+import com.kavish.tutorials.banking.repository.ExpenseRepository;
+import com.kavish.tutorials.banking.repository.IncomeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CashFlowServiceImpl implements CashFlowService{
+    @Autowired
+    private IncomeRepository incomeRepository;
+    @Autowired
+    private ExpenseRepository expenseRepository;
     @Override
     public List<Income> getIncomes() throws Exception {
-        return null;
+        return incomeRepository.getIncomes();
     }
 
     @Override
     public List<Expense> getExpenses() throws Exception {
-        return null;
+        return expenseRepository.getExpenses();
     }
 }
